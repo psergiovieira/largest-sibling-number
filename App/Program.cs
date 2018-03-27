a290bd91e4258ed25e8b17f683c2ab4f771628c8
Paulo Sérgio <psvieira.ti@gmail.com>
2018-03-27 20:02:21 -0300

    criando uma aplicação para utilização da função

--- App/Program.cs
+++ App/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathCode;
 
 namespace App
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int userInput;
+            do
+            {
+               userInput  = Calculate();
+            } while (userInput != -1);
+        }
+
+        private static int Calculate()
+        {
+            Console.WriteLine("Please enter a number to discover your largest sibling number or -1 to exit");
+            var result = Console.ReadLine();
+            var inputNumber = Convert.ToInt32(result);
+            
+            if (inputNumber != -1)
+            {
+                var siblingNumber = new SiblingNumber();
+                var largestSiblingNumber = siblingNumber.Largest(inputNumber);
+                Console.WriteLine($"The sibling number is {largestSiblingNumber}");
+                return largestSiblingNumber;
+            }
+
+            return inputNumber;
         }
     }
 }
